3a67018b3704c69ef564e659bb8b275dbc6712c3
Zahidul-Islam94 <72647292+Zahidul-Islam94@users.noreply.github.com>
2021-12-26 16:44:15 +0600

    model controller create

---
+++ app/Models/LaravelPracties.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LaravelPracties extends Model
+{
+    protected  $guarded=[];
+    use HasFactory;
+}
